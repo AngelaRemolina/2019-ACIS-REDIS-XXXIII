b8b0cb0e2a940f7bb5e1a8a9ae34df72ec29a5ff
angelasofiaremolinagutierrez <angelasofiaremolinagutierrez@gmail.com>
2020-03-16 17:30:53 -0500

    Solución 'game'

--- src/EnProceso/game.java
+++ src/EnProceso/game.java
@@ -1,7 +1,35 @@
 package EnProceso;
 
+import java.util.Scanner;
+
 public class game {
     public static void main(String[] args) {
-        // to do
+        Scanner scan = new Scanner(System.in);
+        int g = Integer.parseInt(scan.nextLine());
+        for (int i = 0; i < g; i++) {
+            int n = Integer.parseInt(scan.nextLine());
+            String[] line = scan.nextLine().split(" ");
+            int a = Integer.parseInt(line[0]);
+            int c = Integer.parseInt(line[1]);
+            String starterName = scan.nextLine();
+
+            if(starterName.equals("Alice")){
+                if(a>c){
+                    System.out.println("ALICE");
+                }
+                else {
+                    System.out.println("CARL");
+                }
+            }
+            else{
+                if(c>a){
+                    System.out.println("CARL");
+                }
+                else{
+                    System.out.println("ALICE");
+                }
+
+            }
+        }
     }
 }
